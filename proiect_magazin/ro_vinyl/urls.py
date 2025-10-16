288203e6eaf0b23ebbfa65d0e9c935a8750b6888
vladbxb <vladbxb@gmail.com>
2025-10-16 09:01:46 +0300

    am facut schimbarile din taskul 2

--- proiect_magazin/ro_vinyl/urls.py
+++ proiect_magazin/ro_vinyl/urls.py
@@ -6,10 +6,19 @@ from . import rute
 from . import views
 # We tie the index request from views.py to the /index endpoint
 urlpatterns = [
-    path('', rute.index, name='index'),
+    path('', views.index, name='index'),
+    path('/', views.index, name='index'),
+    path('despre', views.despre, name='despre'),
+    path('catalog', views.catalog, name='catalog'),
+    path('contact', views.contact, name='contact'),
+    path('info', rute.info, name="info"),
     path('info/', rute.info, name="info"),
+    path('info/data', rute.static_data, name="static_data"),
     path('info/data/', rute.static_data, name="static_data"),
     re_path(r'^info/data/(zi|timp)$', rute.afis_data, name="afis_data"),
+    re_path(r'^info/data/(zi|timp)/$', rute.afis_data, name="afis_data"),
+    path('log', rute.log, name="static_log"),
     path('log/', rute.log, name="static_log"),
-    path('exemplu_simplu/', views.afis_template, name='afis_template'),
+    path('exemplu_simplu', views.exemplu_afis_template, name='afis_template'),
+    path('exemplu_simplu/', views.exemplu_afis_template, name='afis_template'),
 ]
--- proiect_magazin/ro_vinyl/views.py
+++ proiect_magazin/ro_vinyl/views.py
@@ -1,16 +1,32 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def afis_template(request):
+def exemplu_afis_template(request):
     return render(request,"ro_vinyl/simplu.html",
         {
             "param":"valoare",
         }
     )
 
-
 # Create your views here.
 def index(request):
     """Ruta principala pentru aplicatia web."""
-    return HttpResponse('Primul raspuns')
+    return render(request,"ro_vinyl/pagina_principala.html",
+        {
+            "param":"valoare",
+        }
+    )
 
+def despre(request):
+    """Ruta pentru pagina despre"""
+    return render(request, "ro_vinyl/despre.html")
+
+def catalog(request):
+    """Ruta pentru pagina catalog"""
+    # pagina este in lucru
+    return render(request, "ro_vinyl/in_lucru.html")
+
+def contact(request):
+    """Ruta pentru pagina contact"""
+    # pagina este in lucru
+    return render(request, "ro_vinyl/in_lucru.html")
